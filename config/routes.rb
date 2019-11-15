70139235bdeadfe8ca99c1805064cbc95a67dae3
Sophie Lejeune <sophie.shadi.lejeune@gmail.com>
2019-11-15 12:18:23 +1100

    fix get method

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   # get 'ingredients/show'
   # get 'ingredients/new'
   # get 'ingredients/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
+  resources :cocktails, only: [:show, :new, :create, :destroy] do
       resources :doses, only: [:show, :new, :create]
     end
     resources :doses, only: [:destroy]
